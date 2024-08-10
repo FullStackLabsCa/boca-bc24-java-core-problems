e36c9e0ebf5bfaf521c9a5bc1ccf5879dbf71719
Mohan <mohan@Saranyas-MacBook-Pro.local>
2024-08-09 22:37:20 -0400

    added solution for palindromeChecker

--- src/problems/PalindromeChecker.java
+++ src/problems/PalindromeChecker.java
@@ -1,7 +1,27 @@
 package problems;
 
 public class PalindromeChecker {
+    public static void main(String[] args) {
+        isPalindrome(null);
+    }
     public static boolean isPalindrome(String madam) {
-        return false;
+        if(madam == null){
+            System.out.println("Provided null as input");
+            return false;
+        }
+
+        String clnStr = madam.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        System.out.println("Input String: " + madam);
+        System.out.println("Cleaned up Input String: " + clnStr);
+
+        int clnstrLength = clnStr.length();
+        for (int i = 0; i < clnstrLength / 2; i++) {
+            if (clnStr.charAt(i) != clnStr.charAt((clnstrLength - 1) - i)) {
+                System.out.println("String : " + madam + " is Not a Palindrome");
+                return false;
+            }
+        }
+        System.out.println("String : " + madam + " is a Palindrome");
+        return true;
     }
 }
