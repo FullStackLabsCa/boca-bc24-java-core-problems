7130effd8c0443be056fae23f2fe7e0639071f23
MaB2210 <mannbhatt.00@gmail.com>
2024-08-10 12:38:12 -0400

    testPalindromeWithMixedCasePalindrome Passed Sucessfully.

--- src/problems/PalindromeChecker.java
+++ src/problems/PalindromeChecker.java
@@ -1,8 +1,18 @@
 package problems;
 
+import java.util.Scanner;
+
 public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter any string to check Palindrome: ");
+        String str = scanner.nextLine();
+//        String lowerstr = str.toLowerCase();
+//        System.out.println(lowerstr);
+    }
     public static boolean isPalindrome(String str){
         String newstr = str.replaceAll("\\s+","");
+        newstr = newstr.toLowerCase();
         int l = 0;
         int r = newstr.length()-1;
 
