93377f5c80df416c63f7235cdb681c72e28ca4ff
Dhruv.Desai@fullstacklabs.digital <dhruvdesai770@gmail.com>
2024-08-11 10:40:19 -0400

    Passed all the test cases for Finding maxinum in an array

---
+++ src/problems/FindMaximumInArray.java
@@ -0,0 +1,35 @@
+package problems;
+
+import java.util.Scanner;
+
+public class FindMaximumInArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of elements you would like to add in an array: ");
+        String n = scanner.nextLine();
+
+        int[] array = new int[Integer.parseInt(n)];
+
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < Integer.parseInt(n); i++) {
+            array[i] = Integer.parseInt(scanner.nextLine());
+        }
+
+        int maxValue = findMax(array);
+        System.out.println("The maximum value in the array is: " + maxValue);
+    }
+
+    public static int findMax(int[] numbers) {
+        if (numbers == null || numbers.length == 0) {
+            return Integer.MIN_VALUE;
+        }
+
+        int max = numbers[0];
+        for (int j = 0; j < numbers.length; j++) {
+            if (numbers[j] > max) {
+                max = numbers[j];
+            }
+        }
+        return max;
+    }
+}
