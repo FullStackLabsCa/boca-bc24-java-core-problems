11f4f2849d39047dedd8f106ddc9e5c6a9bdef87
Jay Shah <shahjay489@gmail.com>
2024-08-11 15:00:58 -0400

    Find maximum array completed.

--- src/problems/FindMaximumInArray.java
+++ src/problems/FindMaximumInArray.java
@@ -1,5 +1,33 @@
 package problems;
 
+import java.util.Scanner;
+
 public class FindMaximumInArray {
 
+    public static void main(String[] args) {
+
+//
+//        System.out.println("Please enter numbers in");
+//        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = {1,2,3,4};
+
+        int max = findMax(numbers);
+        System.out.println("The maximum value in the array is: " + max);
+
+    }
+
+    public static int findMax(int[] numbers){
+        if (numbers == null || numbers.length == 0){
+            return Integer.MIN_VALUE;
+        }
+        int max = numbers[0];
+
+        for (int i=1; i<numbers.length; i++){
+            if (numbers[i] > max){
+                max = numbers[i];
+            }
+        }
+        return max;
+    }
 }
