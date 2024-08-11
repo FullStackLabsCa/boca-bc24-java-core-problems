e1f414eff3fa426a8d9a9c991385fcb7c9eb0ed7
Jay Shah <shahjay489@gmail.com>
2024-08-11 16:14:57 -0400

    add PalindromeChecker variable of Plaindrome.java class which called isPalindrome method. add logic in java class and run all test and get all green flag of tests.

--- src/problems/Palindrome.java
+++ src/problems/Palindrome.java
@@ -1,4 +1,33 @@
 package problems;
 
 public class Palindrome {
+
+    public static void main(String[] args) {
+
+        String input = "12321";
+        boolean result = isPalindrome(input);
+        System.out.println("Is palindrome " + result);
+    }
+    public static boolean isPalindrome(String input){
+
+        if(input == null){
+            return false;
+        }
+
+        String inputForPalindrome = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        int left = 0;
+        int right = inputForPalindrome.length() - 1;
+
+        while (left<right){
+            if(inputForPalindrome.charAt(left) != inputForPalindrome.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+
+    }
+
 }
