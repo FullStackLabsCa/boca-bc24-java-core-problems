5fa274f93e4a81ed26de66de4cc5addfdf1eeef0
Jay Shah <shahjay489@gmail.com>
2024-08-12 10:32:14 -0400

    Calaulator Program Completed

---
+++ src/problems/Calculator.java
@@ -0,0 +1,66 @@
+package problems;
+
+import com.sun.source.doctree.BlockTagTree;
+
+import java.lang.invoke.SwitchPoint;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter string (ex. 2 + 5): ");
+        String inputString  = scanner.nextLine();
+
+        Calculator calculator = new Calculator();
+        String result  = calculator.calculate(inputString);
+        System.out.println(result);
+
+
+    }
+
+
+    public String calculate(String input) {
+
+        if(input == null || input.trim().isEmpty()){
+            return "Error: Input is empty or null";
+        }
+
+        String[] parts = input.split(" ");
+        if (parts.length !=  3){
+            return "Error: Invalid input format";
+        }
+
+        String operator = parts[1];
+        double num1,num2;
+
+        try{
+            num1 = Double.parseDouble(parts[0]);
+            num2 = Double.parseDouble(parts[2]);
+        }catch (NumberFormatException e){
+            return "Error: Invalid number format";
+        }
+
+        switch (operator){
+            case "+":
+                return String.valueOf(num1 + num2);
+            case "-":
+                return String.valueOf(num1 - num2);
+            case "*":
+                return String.valueOf(num1 * num2);
+            case "/":
+
+                if(num2==0){
+                    return "Error: Cannot divide by zero";
+                }
+                return String.valueOf(num1 / num2);
+
+            default:
+                return "Invalid operator";
+
+        }
+
+    }
+}
