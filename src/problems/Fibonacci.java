97ee3c3574427f360b0e0e540dba1b7fcfcc91d6
Shifa Kajal <Shifa.Kajal@fullstacklabs.digital>
2024-08-20 13:12:03 -0400

    Testing git

--- src/problems/Fibonacci.java
+++ src/problems/Fibonacci.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Fibonacci {
     public static void main(String[] args) {
-
+        System.out.println("Hello World");
     }
 
     private static boolean isValidInput(String input) {
