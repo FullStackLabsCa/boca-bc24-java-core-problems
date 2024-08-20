78441ee9f13ef94eaa3dd69f99e4fad5170b4299
Gaurav Manchanda <gaurav.manchanda@fullstacklabs.digital>
2024-08-20 13:40:22 -0400

    worked on the merge two arrays problem

--- src/problems/array/MergeTwoArrays.java
+++ src/problems/array/MergeTwoArrays.java
@@ -3,6 +3,7 @@ package problems.array;
 import java.util.Arrays;
 import java.util.Scanner;
 
+@SuppressWarnings("java:S106")
 public class MergeTwoArrays {
     public static void main(String[] args) {
         boolean isValueNull = false;
@@ -11,14 +12,14 @@ public class MergeTwoArrays {
         //first Array
         System.out.println("Please enter a values for first array...");
         String firstArrayValue = input.nextLine();
+        //null check
         if (firstArrayValue == "") {
             isValueNull = true;
             System.out.println("Please enter a valid values for first array...");
         }
 
         int[] firstArray = splitAndConvertToNumbersArray(firstArrayValue);
-
-        System.out.println(Arrays.toString(firstArray));
+//        System.out.println("firstArray: "+ Arrays.toString(firstArray));
 
         //second Array
         System.out.println("Please enter a values for second array...");
@@ -29,11 +30,12 @@ public class MergeTwoArrays {
         }
 
         int[] secondArray = splitAndConvertToNumbersArray(secondArrayValue);
+//        System.out.println("secondArray: "+ Arrays.toString(secondArray));
 
-        System.out.println(Arrays.toString(secondArray));
+        int[] mergedArray = mergeArrays(firstArray, secondArray);
 
 //        String longestWord = mergeArrays(value);
-        if (!isValueNull) System.out.println("Capitalize Words: ");
+        if (!isValueNull) System.out.println("Merged Array: " + Arrays.toString(mergedArray));
     }
 
     public static int[] splitAndConvertToNumbersArray(String str) {
@@ -47,7 +49,17 @@ public class MergeTwoArrays {
     }
 
     public static int[] mergeArrays(int[] array1, int[] array2) {
-        int[] mergedArray = new int[5];
+        int arrayLength = array1.length + array2.length;
+        int[] mergedArray = new int[arrayLength];
+
+        for (int i = 0; i < array1.length; i++) {
+            mergedArray[i] = array1[i];
+        }
+
+        for (int j = array1.length; j < mergedArray.length; j++) {
+            mergedArray[j] = array2[j-array1.length];
+        }
+
         return mergedArray;
     }
 }
