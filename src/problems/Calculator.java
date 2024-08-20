b4b20dd7fd49dcdcf805b2f58899c41f4dada014
surajadhikari3 <suraj.adhikari@fullstacklabs.digital>
2024-08-20 12:28:52 -0400

    refactor

--- src/problems/Calculator.java
+++ src/problems/Calculator.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Calculator {
     public String calculate(String userInput) {
-
         try {
             if (userInput == null || userInput.trim().isEmpty()) return "Error: Input is empty or null";
             char operation = 0;
