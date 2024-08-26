3672e38b94ea85868b32d0eb979ba2bfc56c82f9
Anant Jain <Anant.Jain@MacBook-Pro-6.local>
2024-08-26 10:54:59 -0400

    solved bracket matching with stack

--- src/problems/collections/BracketMatchingWithStack.java
+++ src/problems/collections/BracketMatchingWithStack.java
@@ -1,4 +1,28 @@
 package problems.collections;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class BracketMatchingWithStack {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine().trim();
+        String regex = "\\{\\[\\(]\\)}";
+        Stack<Character> brackets = new Stack<>();
+        if (str.length() % 2 == 0) {
+            str = str.replaceAll(regex, "");
+            for(int i=0; i<str.length(); i++){
+                if(str.charAt(i) == '[' || str.charAt(i) == '{' || str.charAt(i) == '('){
+                    brackets.push(str.charAt(i));
+                } else if((str.charAt(i) == ']' && brackets.peek() == '[')
+                        || (str.charAt(i) == '}' && brackets.peek() == '{')
+                        || (str.charAt(i) == ')' && brackets.peek() == '(')){
+                    brackets.pop();
+                } else break;
+            }
+            System.out.println(brackets.isEmpty());
+        } else System.out.println(false);
+    }
 }
