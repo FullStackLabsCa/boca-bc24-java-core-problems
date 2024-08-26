a143dc09941987a7f08f7d17cd3da5703f69ce02
AnilKumar Mummadisetti <AnilKumar.Mummadisetti@MacBook-Pro.local>
2024-08-26 11:06:54 -0400

    problems 11 PalindromeChecker

---
+++ src/problems/array/PalindromeChecker.java
@@ -0,0 +1,26 @@
+package problems.array;
+
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal, Panama"));
+    }
+    public static boolean isPalindrome(String input) {
+        if (input != null) {
+            String reversedString = "";
+            for (int i = 0; i < input.length(); i++) {
+                reversedString = (input.charAt(i)) + reversedString;
+            }
+            if (formatString(input).equalsIgnoreCase(formatString(reversedString))) {
+                return true;
+            } else {
+                return false;
+            }
+        }else {
+            return false;
+        }
+    }
+    public static String formatString(String input){
+        return input.toLowerCase().replaceAll("[^A-Za-z0-9]","");
+    }
+}
