a6bfc0ff2f205586162370da83d8e05b0c69c9ad
Manpreet Kaur <Manpreet.Kaur@MacBook-Pro.local>
2024-08-26 20:51:48 -0400

    addded more problems

---
+++ src/MathematicsProblem/IndirectSubtraction.java
@@ -0,0 +1,20 @@
+package MathematicsProblem;
+
+import java.util.Scanner;
+
+public class IndirectSubtraction {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Enter second number");
+        int sub = 0;
+        int secondNumber = scanner.nextInt();
+        for (int i = 0; i <= secondNumber; i++) {
+            sub = i - firstNumber;
+        }
+        System.out.println("sum of numbers are : " + sub);
+    }
+}
