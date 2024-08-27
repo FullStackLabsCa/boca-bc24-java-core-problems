ce64660731452133e4c334e81f5960e0621afa57
Ankit Joshi <Ankit.Joshi@MacBook-Pro.local>
2024-08-27 13:59:12 -0400

    Added First 50 Even Sum and Odd Sum Problem

--- src/problems/PrintFirst50Even.java
+++ src/problems/PrintFirst50Even.java
@@ -3,7 +3,7 @@ package problems;
 public class PrintFirst50Even {
     public static void main(String[] args) {
         System.out.println("First 50 Even Numbers Are");
-        for (int i = 1; i <= 50; i++) {
+        for (int i = 1; i <= 100; i++) {
             if(i%2==0){
                 System.out.println(i);
             }
