df6857f07ee9ad00fe7a1eb8e2a4635145bf6b2c
Gagandeep Kaur <Gagandeep.Kaur@fullstacklabs.digital>
2024-08-27 16:25:26 -0400

    array added

---
+++ src/arrayproblems/CapitalizeArray.java
@@ -0,0 +1,25 @@
+package problemnofour;
+
+import java.util.Scanner;
+
+public class CapitalizeArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String input = scanner.nextLine();
+
+        System.out.println("Capitalized String is: "+capitalizeWords(input));
+    }
+    public static String capitalizeWords(String input) {
+        String capitalString = "";
+        String[] words = input.split(" ");
+
+        for (String word : words) {
+            String character0 = String.valueOf(word.charAt(0)).toUpperCase();
+            word = word.replaceFirst(String.valueOf(word.charAt(0)), character0);
+
+            capitalString = capitalString.concat(word + " ");
+        }
+        return capitalString;
+    }
+}
