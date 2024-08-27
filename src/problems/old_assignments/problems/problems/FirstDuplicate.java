32f31898007cb8b7d0a27105643b1bba8fff895c
Karan Rana <karanrana1538@gmail.com>
2024-08-27 19:44:05 -0400

    finished the Problem: Prime number bwt thwo numbers

--- src/problems/old_assignments/problems/problems/FirstDuplicate.java
+++ src/problems/old_assignments/problems/problems/FirstDuplicate.java
@@ -3,14 +3,16 @@ package problems.old_assignments.problems.problems;
 public class FirstDuplicate {
     //13 Find first duplicate in an array of numbers
     public static void main(String[] args) {
-        int[] intArray = new int[]{5,6,6,5};
+        int[] intArray = new int[]{5, 6, 6, 5};
 
         int i;
-       FIRST: for (i = 0; i < intArray.length; i++) {
-            SECOND: for (int j = i + 1; j < intArray.length; j++) {
+        first:
+        for (i = 0; i < intArray.length; i++) {
+            second:
+            for (int j = i + 1; j < intArray.length; j++) {
                 if (intArray[i] == intArray[j]) {
                     System.out.println(" result is: " + intArray[i]);
-                    break FIRST;
+                    break first;
                 }
             }
         }
--- src/problems/prime_number/PrimeNumber.java
+++ src/problems/prime_number/PrimeNumber.java
@@ -4,16 +4,32 @@ import java.util.Scanner;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-
-acceptInput(2,10);
-acceptInput(4,11);
-    }
-
-    private static void acceptInput(Integer firstNumber, Integer lastNumber) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the First Number: ");
-        firstNumber = scanner.nextInt();
-        System.out.println("Enter the First Number: ");
-        lastNumber = scanner.nextInt();
+        System.out.println("Enter the starting number: ");
+        int start = scanner.nextInt(); // Starting number (inclusive)
+        System.out.println("Enter the ending number: ");
+        int end = scanner.nextInt();   // Ending number (inclusive)
+
+        System.out.println("Prime numbers between " + start + " and " + end + ":");
+
+        for (int i = start; i <= end; i++) {
+            boolean isPrime = true;
+
+            if (i <= 1) {
+                isPrime = false; // 0 and 1 are not prime numbers
+            } else {
+                // Check if i is divisible by any number from 2 to sqrt(i)
+                for (int j = 2; j * j <= i; j++) {
+                    if (i % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(i + ", "); // Print the prime number
+            }
+        }
     }
 }
