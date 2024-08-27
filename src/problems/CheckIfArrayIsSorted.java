d6dc7b44e7b33fcecbc16e767d55cdf1f720d033
Mohankumar Chandrasekaran <Mohankumar.Chandrasekaran@MacBook-Pro.local>
2024-08-26 23:35:37 -0400

    Added solution for check if array is sorted or not

---
+++ src/problems/CheckIfArrayIsSorted.java
@@ -0,0 +1,17 @@
+package problems;
+
+public class CheckIfArrayIsSorted {
+    public static void main(String[] args) {
+        int[] input = {1, 3, 0, 4, 4};
+        System.out.println("Is the input array is sorted : " + isSorted(input));
+    }
+
+    static boolean isSorted(int[] input) {
+        for (int i = 0; i < input.length - 1; i++) {
+            if (input[i] > input[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
