fb0e0e20204cfd5f3c017abfa156a4dfc78f4887
Gurpreet Singh <Gurpreet.Singh@MacBook-Pro.local>
2024-08-27 15:40:11 -0400

    bracket problem without using stack

---
+++ src/collection_problems/BracketBalancingWithoutStack.java
@@ -0,0 +1,49 @@
+package collection_problems;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class BracketBalancingWithoutStack {
+    public static void bracketChecker(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the Bracket to check is it balanced");
+        String keywordInput = scanner.nextLine();
+        int keyboardInputLength = keywordInput.length();
+        String[] keywordInputArray = keywordInput.split("");
+        List<String> bracketsInputList = Arrays.asList(keywordInputArray);
+      FIRST:  if (keyboardInputLength % 2 == 0) {
+            for (int keywordInputIndex = 0; keywordInputIndex < keyboardInputLength/2; keywordInputIndex++) {
+                String bracketIndex = bracketsInputList.get(keywordInputIndex);
+                switch (bracketIndex) {
+                    case "{":
+                        if (bracketsInputList.get(keyboardInputLength - keywordInputIndex - 1).equals("}")) {
+                            break;
+                        } else {
+                            System.out.println("Brackets are not balanced");
+                            break FIRST;
+                        }
+                    case "(":
+                        if (bracketsInputList.get(keyboardInputLength - keywordInputIndex - 1).equals(")")) {
+                            break;
+                        } else {
+                            System.out.println("Brackets are not balanced");
+                            break FIRST;
+                        }
+                    case "[":
+                        if (bracketsInputList.get(keyboardInputLength - keywordInputIndex - 1).equals("]")) {
+                            break;
+                        } else {
+                            System.out.println("Brackets are not balanced");
+                            break FIRST;
+                        }
+                }
+            }
+            System.out.println("Brackets are balanced");
+        } else System.out.println("Brackets are not balanced");
+    }
+
+    public static void main(String[] args) {
+        BracketBalancingWithoutStack.bracketChecker();
+    }
+}
