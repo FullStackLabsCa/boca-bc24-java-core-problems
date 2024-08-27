065a3c265caf29c2c01ff49734181033b88c9176
Ankit Joshi <Ankit.Joshi@MacBook-Pro.local>
2024-08-27 16:14:41 -0400

    Added Sum of Array Solution

---
+++ src/problems/SumAllNumbersInArray.java
@@ -0,0 +1,13 @@
+package problems;
+
+public class SumAllNumbersInArray {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 5, 5};
+        int sum=0;
+        for (int number : numbers) {
+            sum += number;
+        }
+
+        System.out.println("The Sum of All the elements of array is: "+sum);
+    }
+}
