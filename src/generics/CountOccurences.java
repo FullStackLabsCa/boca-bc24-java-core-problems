9a9dbfbb5313cf0f97df9084bb9e80efd9afb3a2
Akshat Singla <Akshat.Singla@MacBook-Pro.local>
2024-08-27 10:53:18 -0400

    checking in solution for OddNumbersinInteger - Generics Problem 5. Status - Working

--- src/generics/CountOccurences.java
+++ src/generics/CountOccurences.java
@@ -4,13 +4,21 @@ public class CountOccurences {
 
     public static <T> int countOccurences(T[] array, T element){
         int occurences = 0;
-
         for( T arrayElement : array){
             if(arrayElement.equals(element)){ // Using .equals because I want to check the hashCode / Value being equal or not rather than the memory reference
                 occurences++;
             }
         }
-
         return occurences;
+    }
+
+    public static void main(String[] args) {
+        Number[] myList = new Integer[3];
+
+        myList[0] = 1;
+        myList[1] = 2;
+        myList[2] = 2;
+
+        System.out.println(CountOccurences.countOccurences(myList, 2));
     }
 }
--- src/generics/OddNumbersInInteger.java
+++ src/generics/OddNumbersInInteger.java
@@ -1,4 +1,34 @@
 package generics;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class OddNumbersInInteger {
+
+    public static <T extends Integer> int countOdd(Collection<T> collectionOfIntegers ){
+
+        int oddCount = 0;
+
+        for(T element : collectionOfIntegers){
+            if(element.doubleValue() % 2 == 1){
+                oddCount++;
+            }
+        }
+
+        return oddCount;
+    }
+
+
+    public static void main(String[] args) {
+        List<Integer> listOfIntegers = new ArrayList<>();
+
+        listOfIntegers.add(3);
+        listOfIntegers.add(2);
+        listOfIntegers.add(2);
+        listOfIntegers.add(11);
+        listOfIntegers.add(2);
+        listOfIntegers.add(7);
+        System.out.println(OddNumbersInInteger.countOdd(listOfIntegers));
+    }
 }
