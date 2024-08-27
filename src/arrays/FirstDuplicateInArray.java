0d6cb2cf763d42489f830d6d215ab18c2c4461ec
Ankit Joshi <Ankit.Joshi@MacBook-Pro.local>
2024-08-27 15:38:48 -0400

    Added solution for find the first Duplicate in an Array

---
+++ src/arrays/FirstDuplicateInArray.java
@@ -0,0 +1,31 @@
+package arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FirstDuplicateInArray {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6};
+
+        findFirstduplicate(numbers);
+
+    }
+
+    private static void findFirstduplicate(int[] numbers) {
+        Set<Integer> duplicates = new HashSet<>();
+        boolean duplicate = false;
+        for(int number: numbers){
+            if(duplicates.contains(number)){
+                System.out.println("The First Duplicate is "+number);
+                duplicate = true;
+                break;
+            }else{
+                duplicates.add(number);
+                duplicate = false;
+            }
+        }
+        if(!duplicate){
+            System.out.println("No Duplicates found");
+        }
+    }
+}
