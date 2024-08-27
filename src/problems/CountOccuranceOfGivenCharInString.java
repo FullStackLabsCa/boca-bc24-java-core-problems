1b01b54fad774b1726601d224635783614328833
Ankit Joshi <Ankit.Joshi@MacBook-Pro.local>
2024-08-27 15:50:49 -0400

    Added solution for Count Occurance of given Char In String

---
+++ src/problems/CountOccuranceOfGivenCharInString.java
@@ -0,0 +1,16 @@
+package problems;
+
+public class CountOccuranceOfGivenCharInString {
+    public static void main(String[] args) {
+        String input = "Ankit like to Code in Java".toLowerCase();
+        char occuranceOf = 'a';
+        int countOfChar =0;
+
+        for (int currentChar = 0; currentChar < input.length(); currentChar++) {
+            if(input.charAt(currentChar) == occuranceOf){
+                countOfChar++;
+            }
+        }
+        System.out.println("Char "+occuranceOf+" count "+countOfChar);
+    }
+}
