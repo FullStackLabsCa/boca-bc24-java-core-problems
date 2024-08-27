6a5ccbec93d8e02433dfaaf30ace04526b3bb7f2
Nimanshukumar Patel <Nimanshukumar.Patel@MacBook-Pro-5.local>
2024-08-27 09:41:01 -0400

    pushing course problem

--- src/generic/Course.java
+++ src/generic/Course.java
@@ -1,11 +1,36 @@
 package generic;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Course <S, G extends Number> {
 
-    private S identifier;
-    private G grades;
+   Map<S, G> studentToGradeMap = new HashMap<>();
 
-    public void addStudents(S student ){
+   public void enrollStudents (S s, G g) {
+       studentToGradeMap.put(s,g);
+   }
+
+   public G getGrade (S s){
+       G gradeOfStudent = studentToGradeMap.get(s);
+       return gradeOfStudent;
+   }
+
+   public void printStudentsAndGrades (){
+       System.out.println(studentToGradeMap);
+   }
+
+    public static void main(String[] args) {
+
+       Course<String, Integer> java2024 = new Course<>();
+       java2024.enrollStudents("Nimanshu", 100);
+       java2024.enrollStudents("Kiran", 80);
+       java2024.enrollStudents("Anant", 101);
+       java2024.enrollStudents("Shifa",110);
+       java2024.enrollStudents("Akshita",null);
+
+        System.out.println(java2024.getGrade("Shifa"));
+        java2024.printStudentsAndGrades();
 
     }
 }
--- src/stringproblems/CountVowels.java
+++ src/stringproblems/CountVowels.java
@@ -1,4 +1,4 @@
-package str;
+package stringproblems;
 import java.util.*;
 
 public class CountVowels {
