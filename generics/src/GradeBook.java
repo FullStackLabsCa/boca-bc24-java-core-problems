2cd79ceefba40f4a44b859836f057053dae9b3d7
Shifa Kajal <Shifa.Kajal@MacBook-Pro.local>
2024-08-27 10:12:42 -0400

    coding problems done so far - 1,2,4,5,6,7,8,9,10,11,14,16,17

---
+++ generics/src/GradeBook.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GradeBook<G extends Number> {
+
+    private List<G> gradeList;
+    List list = new ArrayList();
+
+
+    public void add(G){
+        return;
+    }
+
+
+}
