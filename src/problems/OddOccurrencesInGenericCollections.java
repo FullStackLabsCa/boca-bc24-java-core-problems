00a0ec2a4643b18ab16bc02df0e8b1a47b789c1f
Mann Bhatt <Mann.Bhatt@MacBook-Pro.local>
2024-08-27 16:58:08 -0400

    Count Odd Occurrences of any generic collections final done successfully running

--- src/problems/OddOccurrencesInGenericCollections.java
+++ src/problems/OddOccurrencesInGenericCollections.java
@@ -1,26 +1,26 @@
-//package problems;
-//
-//import java.util.ArrayList;
-//import java.util.Collection;
-//import java.util.List;
-//
-//public class OddOccurrencesInGenericCollections {
-//    public static void main(String[] args) {
-//        List<Integer> list = new ArrayList<>();
-//        list.add(1);
-//        list.add(2);
-//        list.add(1);
-//        list.add(3);
-//        System.out.println("Occurences of Odd => " + countOddIntegers(list));
-//    }
-//    public static <T extends Collection<Integer>> int countOddIntegers(T t){
-//        int count = 0;
-//
-//        for(Integer i : t){
-//            if(i%2 != 0){
-//                count ++;
-//            }
-//        }
-//        return count;
-//    }
-//}
+package problems;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class OddOccurrencesInGenericCollections {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(1);
+        list.add(3);
+        System.out.println("Occurences of Odd => " + countOddIntegers(list));
+    }
+    public static <T extends Collection<Integer>> int countOddIntegers(T t){
+        int count = 0;
+
+        for(Integer i : t){
+            if(i%2 != 0){
+                count ++;
+            }
+        }
+        return count;
+    }
+}
