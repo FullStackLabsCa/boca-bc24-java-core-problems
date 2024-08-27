534962116f54968a6c844d2a557e83c82ddfbf2b
akshitareactivestax <akshita.bajaj@fullstacklabs.digital>
2024-08-27 13:22:16 -0400

    previous factorial code commit was not according to the given output , this one adheres to the given output

--- src/problems/Factorial.java
+++ src/problems/Factorial.java
@@ -2,19 +2,19 @@ package problems;
 
 // Calculate the factorial of number n (1 >> n <= 12)
 public class Factorial {
-
     public static void main(String[] args) {
+        for (int number = 1; number <= 12; number++) {
+            int factorial = 1;
+            System.out.println("Calculating the factorial of " + number + ":");
 
-        int factorial = 1;
+            for (int i = number; i > 0; i--) {
+                int previousFactorial = factorial;
+                factorial *= i;
+                System.out.println(previousFactorial + " times " + i + " is " + factorial + ".");
+            }
 
-        for (int i = 1 ; i <= 12 ; i++){
-            factorial = factorial * i ;
-
-            System.out.println(factorial);
+            System.out.println("So, the factorial of " + number + " is " + factorial + ".");
+            System.out.println();
         }
-
-
-
     }
-
 }
