15ff58807661f93e53d85b697899f77f2131cf7b
Suraj Adhikari <Suraj.Adhikari@MacBook-Pro.local>
2024-08-27 09:17:51 -0400

    added duplicates in array

--- src/problems/collection/BracketMatchingWithStack.java
+++ src/problems/collection/BracketMatchingWithStack.java
@@ -19,7 +19,19 @@ Input:
 The program should accept a string consisting of only these three types of brackets.
 Output:
 
-The program should return true if the input string is considered valid according to the custom rules, and false otherwise.*/
+The program should return true if the input string is considered valid according to the custom rules, and false otherwise.
+
+Input: {[(])}, Output: true (valid due to the custom rule)
+Input: {[()]}, Output: true (valid due to standard balancing)
+Input: {[}, Output: false (invalid, not balanced)
+Input: {[(])}(), Output: true (valid, combination of twisted and standard sequences)
+Input: [(]), Output: false (invalid, sequence does not match any valid pattern)
+
+
+
+*/
+
+
 
 import java.util.Scanner;
 import java.util.Stack;
@@ -27,33 +39,32 @@ import java.util.Stack;
 public class BracketMatchingWithStack {
 
     public static Boolean bracketMatch(String input) {
-        Boolean isMatched = false;
-        if(input == null || input.length() % 2 != 0 ) return false;
-        if(!(input.contains("[") || input.contains("(") || input.contains("{"))) return false;
+        boolean isMatched = false;
+        if (input == null || input.length() % 2 != 0) return false;
+        if (!(input.contains("[") || input.contains("(") || input.contains("{"))) return false;
         Stack<Character> characterStack = new Stack<>();
         for (int i = 0; i < input.length(); i++) {
-            if(input.charAt(i) =='(' || input.charAt(i) =='{'||input.charAt(i) =='['){
+            if (input.charAt(i) == '(' || input.charAt(i) == '{' || input.charAt(i) == '[') {
                 characterStack.push(input.charAt(i));
             }
 
-            if(input.charAt(i) == ')' || input.charAt(i) == '}' || input.charAt(i) == ']'){
+            if (input.charAt(i) == ')' || input.charAt(i) == '}' || input.charAt(i) == ']') {
 //            if(input.contains(")") || input.contains("}")||input.contains("]")){
-                if(!characterStack.isEmpty()) {
+                if (!characterStack.isEmpty()) {
                     int check = i;
                     Character popElement = characterStack.pop();
                     while (check < input.length()) {
-                        if (bracketMatch(popElement, input.charAt(check))){
+                        if (bracketMatch(popElement, input.charAt(check))) {
                             isMatched = true;
                             break;
-                        }else {
+                        } else {
                             isMatched = false;
                         }
-//                    if(!(input.charAt(i) == characterStack.pop())) {
                         check++;
                     }
                 }
-                }
             }
+        }
         return characterStack.isEmpty() && isMatched;
     }
 
@@ -64,7 +75,7 @@ public class BracketMatchingWithStack {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String stringBracketInput = scanner.nextLine();
-        System.out.println("stringBracketMatched = " +bracketMatch(stringBracketInput));
+        System.out.println("stringBracketMatched = " + bracketMatch(stringBracketInput));
 
     }
 
