86ec4e983b398e2c721393e77e647343822b3792
Nimanshukumar Patel <Nimanshukumar.Patel@MacBook-Pro-5.local>
2024-08-27 10:38:31 -0400

    adding countOdd for collection

--- src/generic/CountOddIntegers.java
+++ src/generic/CountOddIntegers.java
@@ -1,4 +1,44 @@
 package generic;
 
+import java.util.*;
+
 public class CountOddIntegers {
+
+    public static int countOdd(Collection<Integer> collection){
+        int count = 0;
+        for(Integer n : collection){
+            if(n % 2 != 0){
+                count++;
+            }
+        }
+        System.out.println("Given collection contains " + count + " Odd Integers");
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        Set<Integer> integersSet = new HashSet<>();
+        integersSet.add(15);
+        integersSet.add(16);
+        integersSet.add(17);
+        integersSet.add(18);
+        integersSet.add(19);
+        integersSet.add(13);
+
+        countOdd(integersSet);
+
+        List<Integer> integersList = new ArrayList<>();
+        integersList.add(1);
+        integersList.add(18);
+        integersList.add(16);
+        integersList.add(45);
+        integersList.add(987);
+        integersList.add(136);
+        integersList.add(45);
+        integersList.add(5);
+        countOdd(integersList);
+
+
+
+    }
 }
--- src/generic/Course.java
+++ src/generic/Course.java
@@ -1,12 +1,13 @@
 package generic;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.SortedMap;
 
 public class Course <S, G extends Number> {
 
-   Map<S, G> studentToGradeMap = new HashMap<>();
+   Map<S, G> studentToGradeMap = new LinkedHashMap<>();
 
    public void enrollStudents (S s, G g) {
        studentToGradeMap.put(s,g);
@@ -15,9 +16,9 @@ public class Course <S, G extends Number> {
    public void getGrade (S s){
        G gradeOfStudent = studentToGradeMap.get(s);
        if(gradeOfStudent == null){
-           System.out.println("Grades for this student has not assigned");
+           System.out.println("Grades for student : " + s + " has not Assigned !!");
        } else {
-           System.out.println(gradeOfStudent);
+           System.out.println("Grades for student : " + s + " => " + gradeOfStudent);
        }
    }
 
@@ -26,15 +27,29 @@ public class Course <S, G extends Number> {
    }
 
     public static void main(String[] args) {
-
        Course<String, Integer> java2024 = new Course<>();
        java2024.enrollStudents("Nimanshu", 100);
        java2024.enrollStudents("Kiran", 80);
        java2024.enrollStudents("Anant", 101);
        java2024.enrollStudents("Shifa",110);
        java2024.enrollStudents("Akshita",null);
+       java2024.enrollStudents("Akshat", 50);
+
+       Course<Integer,Double> java2025Bootcamp = new Course<>();
+       java2025Bootcamp.enrollStudents(1,22.20);
+       java2025Bootcamp.enrollStudents(2,20.58);
+       java2025Bootcamp.enrollStudents(3,25.23);
+       java2025Bootcamp.enrollStudents(4,20.19);
+       java2025Bootcamp.enrollStudents(5,null);
+       System.out.println("===== Grades of course java2025Bootcamp =====");
+
+       java2025Bootcamp.getGrade(5);
+       java2025Bootcamp.printStudentsAndGrades();
+       java2025Bootcamp.getGrade(3);
+       System.out.println("\n\n");
 
 
+       System.out.println("===== Grades of course java2024 =====");
        java2024.getGrade("Akshita");
        java2024.getGrade("Kiran");
        java2024.printStudentsAndGrades();
