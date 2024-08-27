d92dc55343c0c14363bd2be13d77e991e36ecaf8
Gurpreet Singh <Gurpreet.Singh@fullstacklabs.digital>
2024-08-27 17:19:49 -0400

    Input validation added in collection bracket problems

--- src/collection_problems/BracketBalancingUsingStack.java
+++ src/collection_problems/BracketBalancingUsingStack.java
@@ -4,41 +4,61 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class BracketBalancingUsingStack {
-    public static boolean bracketChecker(String brackerassigned) {
-        Stack<String> stringStack = new Stack<>();
-        char characterAt;
+    public static boolean validateInput(String userInput){
+        boolean isValid =false;
+        String[] userInputSplit = userInput.split("");
+        FIRST: for (String input: userInputSplit){
+            switch (input){
+                case "{":
+                case "}":
+                case "(":
+                case ")":
+                case "[":
+                case "]":
+                    isValid=true;
+                    break;
+                default:
+                    break FIRST;
+            }
+        }
+        return isValid;
+    }
+    public static boolean bracketChecker() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the Bracket to check is it balanced");
+        String input = scanner.nextLine();
         boolean isBracketedPair = false;
-        if (brackerassigned.length() % 2 == 0) {
-//            if (brackerassigned.charAt(0)==brackerassigned.charAt(brackerassigned.length()-1)) {
-            for (int bracketArrayIndex = 0; bracketArrayIndex < brackerassigned.length(); bracketArrayIndex++) {
-                characterAt = brackerassigned.charAt(bracketArrayIndex);
-                if ((characterAt == '{') || (characterAt == '(') || ((characterAt == '['))) {
-                    stringStack.push(Character.toString(characterAt));
-                }
-                if ((characterAt == '}') || (characterAt == ')') || ((characterAt == ']'))) {
-                    if (stringStack.empty()) {
-                        isBracketedPair = false;
-                        break;
-                    } else {
-                        char top = stringStack.pop().charAt(0);
+        if (validateInput(input)) {
+            Stack<String> stringStack = new Stack<>();
+            char characterAt;
+            if (input.length() % 2 == 0) {
+                for (int bracketArrayIndex = 0; bracketArrayIndex < input.length(); bracketArrayIndex++) {
+                    characterAt = input.charAt(bracketArrayIndex);
+                    if ((characterAt == '{') || (characterAt == '(') || ((characterAt == '['))) {
+                        stringStack.push(Character.toString(characterAt));
+                    }
+                    if ((characterAt == '}') || (characterAt == ')') || ((characterAt == ']'))) {
+                        if (stringStack.empty()) {
+                            isBracketedPair = false;
+                            break;
+                        } else {
+                            char top = stringStack.pop().charAt(0);
 
-                        if (((characterAt == '}') && (top == '{')) || ((characterAt == ']') && (top == '['))
-                                || ((characterAt == ')') && (top == '('))
-                        ) {
-                            isBracketedPair = true;
+                            if (((characterAt == '}') && (top == '{')) || ((characterAt == ']') && (top == '['))
+                                    || ((characterAt == ')') && (top == '('))
+                            ) {
+                                isBracketedPair = true;
+                            }
                         }
                     }
                 }
             }
-//            }else isBracketedPair =false;
-        } else isBracketedPair = false;
+        }else System.out.println("Invalid Input");
         return isBracketedPair;
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to bracket Balancer");
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println("Is the Bracket exist in pair and correct order : " + bracketChecker(input));
+        System.out.println("Is the Bracket exist in pair and correct order : " + BracketBalancingUsingStack.bracketChecker());
     }
 }
--- src/collection_problems/BracketBalancingWithoutStack.java
+++ src/collection_problems/BracketBalancingWithoutStack.java
@@ -5,44 +5,72 @@ import java.util.List;
 import java.util.Scanner;
 
 public class BracketBalancingWithoutStack {
-    public static void bracketChecker(){
+
+    public static boolean validateInput(String userInput){
+        boolean isValid =false;
+        String[] userInputSplit = userInput.split("");
+        String[] restrictedInput ={"{","}","(",")","[","]"};
+        FIRST: for (String input: userInputSplit){
+//            for (String requiredinput :restrictedInput)
+//            if (input.equals(requiredinput)){
+//                isValid=true;
+//                break;
+//            }
+            switch (input){
+                case "{":
+                case "}":
+                case "(":
+                case ")":
+                case "[":
+                case "]":
+                    isValid=true;
+                    break;
+                default:
+                    break FIRST;
+            }
+        }
+        return isValid;
+    }
+    public static void bracketChecker() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the Bracket to check is it balanced");
         String keywordInput = scanner.nextLine();
-        int keyboardInputLength = keywordInput.length();
-        String[] keywordInputArray = keywordInput.split("");
-        List<String> bracketsInputList = Arrays.asList(keywordInputArray);
-      FIRST:  if (keyboardInputLength % 2 == 0) {
-            for (int keywordInputIndex = 0; keywordInputIndex < keyboardInputLength/2; keywordInputIndex++) {
-                String bracketIndex = bracketsInputList.get(keywordInputIndex);
-                switch (bracketIndex) {
-                    case "{":
-                        if (bracketsInputList.get(keyboardInputLength - keywordInputIndex - 1).equals("}")) {
-                            break;
-                        } else {
-                            System.out.println("Brackets are not balanced");
-                            break FIRST;
-                        }
-                    case "(":
-                        if (bracketsInputList.get(keyboardInputLength - keywordInputIndex - 1).equals(")")) {
-                            break;
-                        } else {
-                            System.out.println("Brackets are not balanced");
-                            break FIRST;
-                        }
-                    case "[":
-                        if (bracketsInputList.get(keyboardInputLength - keywordInputIndex - 1).equals("]")) {
-                            break;
-                        } else {
-                            System.out.println("Brackets are not balanced");
-                            break FIRST;
-                        }
+        if (validateInput(keywordInput)) {
+            int keyboardInputLength = keywordInput.length();
+            String[] keywordInputArray = keywordInput.split("");
+            List<String> bracketsInputList = Arrays.asList(keywordInputArray);
+            FIRST:
+            if (keyboardInputLength % 2 == 0) {
+                for (int keywordInputIndex = 0; keywordInputIndex < keyboardInputLength / 2; keywordInputIndex++) {
+                    String bracketIndex = bracketsInputList.get(keywordInputIndex);
+                    switch (bracketIndex) {
+                        case "{":
+                            if (bracketsInputList.get(keyboardInputLength - keywordInputIndex - 1).equals("}")) {
+                                break;
+                            } else {
+                                System.out.println("Brackets are not balanced");
+                                break FIRST;
+                            }
+                        case "(":
+                            if (bracketsInputList.get(keyboardInputLength - keywordInputIndex - 1).equals(")")) {
+                                break;
+                            } else {
+                                System.out.println("Brackets are not balanced");
+                                break FIRST;
+                            }
+                        case "[":
+                            if (bracketsInputList.get(keyboardInputLength - keywordInputIndex - 1).equals("]")) {
+                                break;
+                            } else {
+                                System.out.println("Brackets are not balanced");
+                                break FIRST;
+                            }
+                    }
                 }
-            }
-            System.out.println("Brackets are balanced");
-        } else System.out.println("Brackets are not balanced");
+                System.out.println("Brackets are balanced");
+            } else System.out.println("Brackets are not balanced");
+        }else System.out.println("Invalid Input");
     }
-
     public static void main(String[] args) {
         BracketBalancingWithoutStack.bracketChecker();
     }
