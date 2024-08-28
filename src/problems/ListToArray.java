6d2b73b21b07efb2bdec54afdabd774e75db239f
Mann Bhatt <Mann.Bhatt@MacBook-Pro.local>
2024-08-28 10:25:03 -0400

    All tewt cases passed successfull for ListToArrayTest including testCountOccurrencesWithIntegers and testCountOccurrencesWithStrings using the newInstance of array with the size of list

--- src/problems/ListToArray.java
+++ src/problems/ListToArray.java
@@ -1,9 +1,23 @@
 package problems;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 //PROBLEM : GENERIC METHOD TO CONVERT LIST TO ARRAY
 public class ListToArray {
+    public static void main(String[] args) {
+        List<String> str = new ArrayList<>();
+        str.add("Mann");
+        str.add("Ashish");
+        str.add("Bhatt");
+
+        System.out.println(Arrays.toString(listToArray(str,new String[str.size()])));
+
+    }
+
+
 
     public static <T> T[] listToArray(List<T> list, T[] array){
         if(list == null){
@@ -12,11 +26,20 @@ public class ListToArray {
         if (list.equals(Collections.emptyList())){
             return null;
         }
-        Object[] tempArray = new Object[list.size()];
-        for(int i = 0; i< list.size();i++){
-            tempArray[i] = list.get(i);
+        if(array == null){
+            System.out.println("Array cannot be null!!!");
         }
-        return (T[])tempArray;
+        int size = list.size();
+        if (array.length < size) {
+
+            array = (T[]) Array.newInstance(array.getClass().getComponentType(), size);
+        }
+//        System.out.println(array.getClass().getComponentType());
+
+        for(int i = 0; i< list.size();i++){
+            array[i] = list.get(i);
+        }
+        return array;
 
     }
 }
