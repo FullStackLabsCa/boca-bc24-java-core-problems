bcd7024942190d0ac18719997a485d13319d5692
Dhruv Patel <Dhruv.Patel@MacBook-Pro.local>
2024-08-28 09:50:42 -0400

    Update the ListToArray as it Pass all the test cases

--- src/problems/genrics/ListToArray.java
+++ src/problems/genrics/ListToArray.java
@@ -5,22 +5,29 @@ import java.util.List;
 
 public class ListToArray {
 
-    public static void main(String[] args) {
-        List<Integer> list = List.of(1, 2, 3, 4, 5);
+//    public static void main(String[] args) {
+//        List<Integer> list = List.of(1, 2, 3, 4, 5);
+//
+//
+//        Integer[] newArray = listToArray(list);
+//        System.out.println("newArray = " + newArray);
+//    }
 
 
-        Integer[] newArray = listToArray(list);
-        System.out.println("newArray = " + newArray);
-    }
+    public static <T> T[] listToArray(List<T> list,T[] array) {
 
-
-    public static <T> T[] listToArray(List<T> list) {
-        T[] array =(T[]) new Object [list.size()];
-
-        for (int i = 0; i < list.size(); i++) {
-            array[i] = list.get(i);
+        if (list == null || list.isEmpty()){
+            return null;
 
         }
-        return array;
+        else {
+
+            for (int i = 0; i < list.size(); i++) {
+                array[i] = list.get(i);
+
+            }
+            return array;
+        }
+
     }
 }
