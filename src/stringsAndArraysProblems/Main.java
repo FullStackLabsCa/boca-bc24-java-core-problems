f5956491ca1307b1481db68313f5b949658bdbd8
akshitareactivestax <akshita.bajaj@fullstacklabs.digital>
2024-08-28 13:23:42 -0400

    Find duplicates in an Array using Arraylist and then converting the dulpicatelist into array

--- src/stringsAndArraysProblems/Main.java
+++ src/stringsAndArraysProblems/Main.java
@@ -15,24 +15,34 @@ public class Main {
         //  int vowelCount = CountVowelsInString.countVowels(inputString);
         //  System.out.println("number of vowels : " + vowelCount);
 
-         //String newStrWithoutVowels = RemoveVowelsInString.removeVowels(inputString);
-         //System.out.println(" new String: " + newStrWithoutVowels);
+        //String newStrWithoutVowels = RemoveVowelsInString.removeVowels(inputString);
+        //System.out.println(" new String: " + newStrWithoutVowels);
 
 
         //LongestWordInString.findLongestWordInString(inputString);
 
-       // CapitalFirstWordOfString.capitalizeWord(inputString);
+        // CapitalFirstWordOfString.capitalizeWord(inputString);
 
-       // System.out.println(SortedArray.isSorted(new int[]{2, 9,8, 5, 6, 6}));
+        // System.out.println(SortedArray.isSorted(new int[]{2, 9,8, 5, 6, 6}));
 
-        int[] mergedArray = (MergeTwoArrays.mergeArray(new int[]{2, 9,8, 5, 6, 6}, new int[]{233,22,24}));
-        for (int i = 0; i < mergedArray.length; i++) {
-            System.out.print(mergedArray[i]);
-            if (i < mergedArray.length - 1) {
+
+//        int[] mergedArray = (MergeTwoArrays.mergeArray(new int[]{2, 9,8, 5, 6, 6}, new int[]{233,22,24}));
+//        for (int i = 0; i < mergedArray.length; i++) {
+//            System.out.print(mergedArray[i]);
+//            if (i < mergedArray.length - 1) {
+//                System.out.print(", ");
+//            }
+//        }
+
+
+        int[] arrayOfDupes = (DuplicateNumInArray.findDuplicateNum(new int[]{2, 9 ,9 , 9  , 6, 6}));
+        for (int i = 0; i < arrayOfDupes.length; i++) {
+            System.out.print(arrayOfDupes[i]);
+            if (i < arrayOfDupes.length - 1) {
                 System.out.print(", ");
             }
         }
 
-        }
-
     }
+
+}
