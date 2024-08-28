1f6751e0c17668442f1a51a4721fcb7eed3fcaed
Mann Bhatt <Mann.Bhatt@MacBook-Pro.local>
2024-08-28 09:25:50 -0400

    testCountOccurencesWithDoubles passed using .equals() method and debugginh the testCountOccurrencesWithNullElement checking null using ==

--- src/problems/CountOccurrences.java
+++ src/problems/CountOccurrences.java
@@ -7,7 +7,12 @@ public class CountOccurrences {
     public static <T> int countOccurrences(T[] array, T element){
         int count = 0;
         for(int i = 0;i< array.length;i++){
-            if(element == array[i]){
+            if(element == null){
+                if(element == array[i]){
+                    count ++;
+                }
+            }
+            else if(element.equals(array[i])){
                 count ++;
             }
         }
