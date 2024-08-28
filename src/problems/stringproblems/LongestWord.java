7499ca33f13db6bb123ddb5f34d37ccde20070b4
Shifa Kajal <Shifa.Kajal@MacBook-Pro.local>
2024-08-28 10:17:31 -0400

    code for string problem 3 finding the longest word in string

--- src/problems/stringproblems/LongestWord.java
+++ src/problems/stringproblems/LongestWord.java
@@ -3,25 +3,32 @@ package problems.stringproblems;
 import java.util.Scanner;
 
 public class LongestWord {
-        public static String findLongestWord(String input) {
-            String[] userSentenceSplit = input.split(" ");
-
-            String longestWord = "";
-            for (int i = 0; i < userSentenceSplit[i].length(); i++) {
-                if (userSentenceSplit[i+1].length() >= userSentenceSplit[i].length()) {
-                }
-                longestWord = userSentenceSplit[i+1];
-                return userSentenceSplit[i+1];
-
-
+    public static String findLongestWord(String input) {
+        String[] userSentenceSplit = input.split(" ");
+        String longestWordInString = "";
+        for (String word : userSentenceSplit) {
+            if (word.length() >= longestWordInString.length()) {
+                longestWordInString = word;
             }
-            return input;
         }
-        public static void main (String[]args){
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter your sentence");
-            String userSentence = scanner.nextLine();
-            System.out.println("The longest word in your sentence is: " + LongestWord.findLongestWord(userSentence));
-        }
+        return longestWordInString;
     }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your sentence");
+        String userSentence = scanner.nextLine();
+        System.out.println("The longest word in your sentence is: " + LongestWord.findLongestWord(userSentence));
+    }
+}
+
+
+//for (int i = 0; i < userSentenceSplit[i].length(); i++) {
+//for(int j = i +1; j < userSentenceSplit[j].length(); j++){
+//if (userSentenceSplit[j].length() >= userSentenceSplit[i].length()) {
+//longestWordInString = userSentenceSplit[j] ;
+
+// }
+//if (userSentenceSplit[i+1].length() >= userSentenceSplit[i].length()) {
+//  longestWordInString = userSentenceSplit[i + 1] + string;
+// }
