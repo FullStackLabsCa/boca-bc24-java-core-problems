8486cdc353211426da4d78d70ed8804b6aa1e42a
Sukhvir.Kaur <Sukhvir.Kaur@fullstacklabs.digital>
2024-08-27 22:32:29 -0400

    factor problem

--- src/problems/Factor.java
+++ src/problems/Factor.java
@@ -1,5 +1,20 @@
 package problems;
 
-public class Factor {
+import java.util.Scanner;
 
+public class Factor {
+    public static void main(String[] args) {
+        System.out.println("Please enter the number to find the factors");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.print("factors are :");
+        for (int i=1;i<=num;i++){
+            if (num%i==0){
+                System.out.print(i);
+                if (i<num)
+                    System.out.print(" , ");
+            }
+        }
+        System.out.println(" .");
+    }
 }
