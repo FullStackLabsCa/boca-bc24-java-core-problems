bb1ca6a8252f5607e52a8f3048a9e8a57b4ce674
Anant Jain <Anant.Jain@MacBook-Pro-6.local>
2024-08-28 09:01:47 -0400

    tests passed for course

---
+++ src/problems/generics/Course.java
@@ -0,0 +1,56 @@
+package problems.generics;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class Course<S, G extends Number> {
+    private final Map<S, G> students;
+
+    public Course() {
+        this.students = new HashMap<>();
+    }
+
+    public void enrollStudent(S studentIdentifier){
+        this.students.put(studentIdentifier, null);
+    }
+
+    public void assignGrade(S studentIdentifier, G grade){
+        if(this.students.containsKey(studentIdentifier)) this.students.replace(studentIdentifier, grade);
+        else System.out.println("Student does not exist.");
+    }
+
+    public Optional<G> getGrade(S studentIdentifier){
+        return Optional.ofNullable(this.students.get(studentIdentifier));
+    }
+
+    public Map<S, G> getAllGrades(){
+        return this.students;
+    }
+
+    public void listAllGrades(){
+        System.out.println(this.students);;
+    }
+
+    public boolean isStudentEnrolled(S student) {
+        return this.students.containsKey(student);
+    }
+
+    public static void main(String[] args) {
+        Course<String, Double> course = new Course<>();
+
+        course.enrollStudent("John");
+        course.enrollStudent("Jason");
+        course.enrollStudent("Jack");
+
+        course.assignGrade("John", 4.0);
+        course.assignGrade("Jason", 3.8);
+        course.assignGrade("Jack", 3.9);
+
+        System.out.println("Grade of John: " + course.getGrade("John"));
+        System.out.println("Grade of Jason: " + course.getGrade("Jason"));
+        System.out.println("Grade of Jack: " + course.getGrade("Jack"));
+
+        System.out.println("All grades: " + course.getAllGrades());
+    }
+}
