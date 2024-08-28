4f40fe5fa040b6da1d9efd5e7d4c5ba7ab8ed5e7
KaurdeepGagan <Gagandeep.Kaur@fullstacklabs.digital>
2024-08-27 21:14:45 -0400

    Added even number

---
+++ src/loopproblems/EvenNumber.java
@@ -0,0 +1,15 @@
+package loopproblems;
+
+public class Main {
+    public static void main(String[] args) {
+        int num =50;
+        int sum = 0;
+        for(int i=1;i<=num*2;i++){
+            if(i%2==0){
+                sum = sum+i;
+                System.out.println("Even Number:" +i);
+            }
+        }
+        System.out.println("Sum Of Even Number: " +sum);
+    }
+}
