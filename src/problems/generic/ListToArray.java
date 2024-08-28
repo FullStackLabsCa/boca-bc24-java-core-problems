5fba859baed58aa6b113e2d18f10b73b32eb61bb
Gaurav Manchanda <gaurav.manchanda@fullstacklabs.digital>
2024-08-28 09:13:33 -0400

    Passed the test cases of ListToArray

--- src/problems/generic/ListToArray.java
+++ src/problems/generic/ListToArray.java
@@ -5,9 +5,10 @@ import java.util.List;
 
 @SuppressWarnings("java:S106")
 public class ListToArray {
-    public static <T> T[] listToArray(List<T> list) {
-        T[] array = (T[]) java.lang.reflect.Array.newInstance(list.get(0).getClass(), list.size());
-
+    public static <T> T[] listToArray(List<T> list, T[] array) {
+        if (list == null || list.isEmpty()) {
+            return null;
+        }
         for (int i = 0; i < list.size(); i++) {
             array[i] = list.get(i);
         }
@@ -16,11 +17,11 @@ public class ListToArray {
 
     public static void main(String[] args) {
         List<Integer> integers = Arrays.asList(1, 2, 3, 4);
-        Integer[] intArrayResult = listToArray(integers);
+        Integer[] intArrayResult = listToArray(integers,new Integer[integers.size()]);
 
         System.out.println("intArrayResult = " + Arrays.toString(intArrayResult));
 
         List<String> string = Arrays.asList("Harry", "Harman", "Naveen");
-        System.out.println("Array of string: " + Arrays.toString(listToArray(string)));
+        System.out.println("Array of string: " + Arrays.toString(listToArray(string , new String[string.size()])));
     }
 }
