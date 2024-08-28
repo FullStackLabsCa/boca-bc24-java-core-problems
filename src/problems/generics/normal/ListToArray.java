59c0fe35988222348db23083e1aadf6bc9ad4d9f
Dhruv Desai <dhruvdesai@770@gmail.com>
2024-08-28 11:31:13 -0400

    Passed 3 out of 5 test cases for ListToArray problem

--- src/problems/generics/normal/ListToArray.java
+++ src/problems/generics/normal/ListToArray.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class ConvertListToArray <T>{
+public class ListToArray<T>{
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
         list.add("Abhay");
