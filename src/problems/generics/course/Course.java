9d23b5ec6bd70d0a46b767ccb89464754d0fdb71
Dhruv Desai <dhruvdesai@770@gmail.com>
2024-08-29 10:14:53 -0400

    Passed all test cases for Course (Generics) problem

--- src/problems/generics/course/Course.java
+++ src/problems/generics/course/Course.java
@@ -1,56 +1,59 @@
-//package problems.generics.course;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//public class Course <S, G extends Number>{
-//        String courseName;
-//        Map<S, G> studentGrades;
-//
-//    public Course(String courseName, Map<S, G> studentGrades) {
-//        this.courseName = courseName;
-//        this.studentGrades = studentGrades;
-//    }
-//
-//    public String getCourseName() {
-//        return courseName;
-//    }
-//
-//    public void addStudent(S studentId, G grade){
-//        studentGrades.put(studentId, grade);
-//    }
-//
-//    public void removeStudent(S studentId){
-//        studentGrades.remove(studentId);
-//    }
-//
-//    public Map<S, G> getStudentGrades() {
-//        return studentGrades;
-//    }
-//
-//    public Map<S, G> getAllGrades() {
-//        return new HashMap<>(studentGrades);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Course{" +
-//                "courseName='" + courseName + '\'' +
-//                ", studentGrades=" + studentGrades +
-//                '}';
-//    }
-//
-//    public static void main(String[] args) {
-//        Course<Integer, Double> mathCourse = new Course<>("Math 101");
-//
-//        mathCourse.addStudent(1, 95.5);
-//        mathCourse.addStudent(2, 88.0);
-//
-//        System.out.println(mathCourse);
-//
-//        mathCourse.removeStudent(2);
-//
-//        System.out.println("Grade for student 1: " + mathCourse.getStudentGrades(1));
-//        System.out.println("All grades: " + mathCourse.getAllGrades());
-//    }
-//}
+package problems.generics.course;
+
+import java.util.*;
+
+public class Course <S, G extends Number>{
+        Map<S, G> studentGrades;
+
+    public Course(String courseName, Map<S, G> studentGrades) {
+        this.studentGrades = studentGrades;
+    }
+
+    public Course() {
+        this.studentGrades = new HashMap<>();
+    }
+
+    public void enrollStudent(S studentId) {
+        studentGrades.put(studentId, null);
+    }
+
+    public boolean isStudentEnrolled(S studentId){
+        return studentGrades.containsKey(studentId);
+    }
+
+    public Optional<G> assignGrade(S studentId, G grade) {
+        if (!studentGrades.containsKey(studentId)) {
+            return Optional.empty();
+        }
+        studentGrades.put(studentId, grade);
+        return Optional.of(grade);
+    }
+
+    public Optional<G> getGrade(S studentId){
+            return Optional.ofNullable(studentGrades.get(studentId));
+    }
+
+    public Map<S,G> getAllGrades(){
+        return studentGrades;
+    }
+
+    public Map<S, G> listAllGrades(){
+        return studentGrades;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "studentGrades=" + studentGrades;
+    }
+
+    public static void main(String[] args) {
+        Course<Integer, Double> mathCourse = new Course<>();
+
+        mathCourse.enrollStudent(1);
+        mathCourse.assignGrade(1, 88.0);
+
+        System.out.println("Grade for student 1: " + mathCourse.getGrade(1));
+        System.out.println("All grades: " + mathCourse.getAllGrades());
+    }
+}
