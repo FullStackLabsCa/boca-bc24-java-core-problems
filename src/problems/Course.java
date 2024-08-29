d5bddd727326218291af46498274f56f93cdb378
Mandeep Kaur <Mandeep.Kaur@MacBook-Pro.local>
2024-08-29 15:11:25 -0400

    pushing WIP files

---
+++ src/problems/Course.java
@@ -0,0 +1,62 @@
+package problems;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class Course<S,G extends Number>{
+
+    private S studentId;
+    private G grades;
+    Map<S, G> studentToGradesMap = new HashMap<>();
+
+
+    public void enrollStudent(S studentId){
+        studentToGradesMap.put(studentId,null);
+    }
+    public void assignGrade(S studentId, G grades){
+        studentToGradesMap.put(studentId,grades);
+//        if(studentToGradesMap.containsKey(studentId)){
+//            studentToGradesMap.put(studentId,grades);
+//        }else{
+//            return "Student with "+ studentId + "is not enrolled yet";
+//        }
+//        return null;
+    }
+    public G retrieveGrades(S studentId){
+        return studentToGradesMap.get(studentId);
+
+    }
+    public void listAllGrade(){
+        studentToGradesMap.values();
+
+    }
+    public void listAllStudent(){
+        if (studentToGradesMap.isEmpty()){
+            System.out.println("No grades");
+        }else{
+            for(Map.Entry<S,G> entry:studentToGradesMap.entrySet()){
+                System.out.println("Student Id: "+ entry.getKey() +" Grades "+ entry.getValue());
+            }
+        }
+    }
+
+
+    public Optional<G> getGrade(S i) {
+            return studentToGradesMap.get(i);
+    }
+
+    public Map<S, G> getAllGrades() {
+            return studentToGradesMap;
+    }
+
+    public void listAllGrades() {
+
+    }
+
+    public boolean isStudentEnrolled(S i) {
+        if(studentToGradesMap.containsKey(i))
+            return true;
+        return false;
+    }
+}
--- src/problems/Fibonacci.java
+++ src/problems/Fibonacci.java
@@ -1,19 +1,23 @@
-//package problems;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import java.util.Scanner;
-//
-//public class Fibonacci {
-//    public static void main(String[] args) {
-//
-//    }
-//
+package problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Integer> generateFibonacci(int i) {
+            return null;
+    }
+
 //    private static boolean isValidInput(String input) {
 //    }
 //
 //    public static List<Integer> generateFibonacci(int n) {
 //    }
-//}
-//
+}
+
