ee421f6f122267768bae937827e47dedc32f351c
Kiran <Kiran.Virani@fullstacklabs.digital>
2024-08-31 12:48:27 -0400

    calculator problem with multiple operations

--- src/problems/calculator/Calculator.java
+++ src/problems/calculator/Calculator.java
@@ -5,13 +5,23 @@ import java.util.Scanner;
 public class Calculator {
     public static boolean isValidString(String expression) {
         boolean isValid = false;
-        if (expression.matches("^\\s*\\d+(\\.\\d+)?\\s*[+\\-*/]\\s*\\d+(\\.\\d+)?\\s*$")) {
+        if (expression.matches("^[-+]?(\\d+(\\.\\d+)?|\\.\\d+)([-+*/^%]([-+]?(\\d+(\\.\\d+)?|\\.\\d+)|\\([-+]?(\\d+(\\.\\d+)?|\\.\\d+)([-+*/^%]([-+]?(\\d+(\\.\\d+)?|\\.\\d+)))*\\)))*$")) {
             isValid = true;
         }
         return isValid;
     }
 
-    public static double performOperation (char operator, double operand1, double operand2) {
+    public static int checkPriority(char operator) {
+        int operatorPriority = switch (operator) {
+            case '*', '/' -> 1;
+            case '+', '-' -> 2;
+            case '^' -> 3;
+            default -> 0;
+        };
+        return operatorPriority;
+    }
+
+    public static double performOperation (char operator, double operand1, double operand2) throws Exception{
         double result = 0;
         switch (operator) {
             case '+':
@@ -25,35 +35,80 @@ public class Calculator {
                 break;
             case '/':
                 if (operand2 == 0) {
-                    System.out.println("Divide by zero exception");
-                    break;
+                    throw new ArithmeticException("Division by zero is not allowed.");
+//                    System.out.println("Division by zero is not allowed.");
+//                    break;
                 }
                 result = operand1 / operand2;
                 break;
             default:
-                System.out.println("Invalid Operation");
+                System.out.println("ERROR: INVALID OPERATION");
                 break;
         }
         return result;
     }
 
-    static double calculate(String expression) {
-        double result = 0.0;
+    public static double calculate(String expression) throws Exception {
+        double [] operands = new double[expression.length()];
+        char [] operators = new char[expression.length()];
+        int operandsIndex = 0, operatorIndex = 0;
 
-        String[] operands = expression.split("[+\\-*/]");
-        String operand1 = operands[0];
-        String operand2 = operands[1];
-        char operator = expression.charAt(operand1.length());
-        System.out.println("Operator=" + operand1.length() + " == " + expression.charAt(operand1.length()));
-        System.out.println("Operands=" + expression);
-        result = performOperation(operator, Double.valueOf(operand1), Double.valueOf(operand2));
+        System.out.println("Expression after removing space: " + expression);
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
 
-        return result;
+            if (ch == '(') {
+                operators[operatorIndex++] = ch;
+            } else if (ch == ')') {
+                if (operatorIndex > 0 && checkPriority(operators[operatorIndex - 1]) <= checkPriority(ch)) {
+                    operandsIndex--;
+                    char operator = operators[--operatorIndex];
+                    operands[operandsIndex - 1] = performOperation(operator, operands[operandsIndex - 1], operands[operandsIndex]);
+                }
+                operatorIndex--;
+            } else if(Character.isDigit(ch) || ch == '.') {
+                StringBuilder sb = new StringBuilder();
+                while (i < expression.length() && (Character.isDigit(expression.charAt(i)) || expression.charAt(i) == '.')) {
+                    sb.append(expression.charAt(i));
+                    i++;
+                }
+                operands[operandsIndex] = Double.parseDouble(sb.toString());
+                operandsIndex++;
+                i--;
+            } else if(String.valueOf(ch).matches("[+*/^-]")) {
+                if (operatorIndex > 0 && checkPriority(operators[operatorIndex - 1]) <= checkPriority(ch)) {
+                    operandsIndex--;
+                    char operator = operators[--operatorIndex];
+                    operands[operandsIndex - 1] = performOperation(operator, operands[operandsIndex - 1], operands[operandsIndex]);
+                }
+                operators[operatorIndex++] = ch;
+            }
+        }
+
+        while (operatorIndex > 0) {
+            operandsIndex--;
+            char operator = operators[--operatorIndex];
+            operands[operandsIndex - 1] = performOperation(operator, operands[operandsIndex - 1], operands[operandsIndex]);
+        }
+
+        return operands[0];
     }
 
-    public static void main(String[] args) {
+    public static void clearMemory() {}
+
+    public static long recallMemory() {
+        return 0;
+    }
+
+    public static void storeInMemory(double v) {}
+
+    public static Object recallAllMemory() {
+        return null;
+    }
+
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("Enter an expression:: ");
+        System.out.println("Enter an expression:: ");
         String expression = scanner.nextLine();
 
         boolean isValidString = isValidString(expression.trim());
@@ -61,6 +116,6 @@ public class Calculator {
             System.out.println("ERROR: INVALID STRING");
             return;
         }
-        System.out.printf("Calculate ===" + calculate(expression.trim()));
+        System.out.printf("Calculate ===" + calculate(expression.replaceAll(" ", "")));
     }
 }
