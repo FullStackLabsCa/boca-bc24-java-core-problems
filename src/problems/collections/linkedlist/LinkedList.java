98aaa72f75d291249e7d7ca08563aacc098dc2d2
Anant Jain <Anant.Jain@MacBook-Pro-6.local>
2024-09-06 11:01:45 -0400

    linked list tests passed

---
+++ src/problems/collections/linkedlist/LinkedList.java
@@ -0,0 +1,88 @@
+package problems.collections.linkedlist;
+
+import java.util.NoSuchElementException;
+
+public class LinkedList {
+
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public void addFirst(int number) {
+        if (head == null) {
+            head = new Node(number);
+        } else {
+            Node current = head;
+            head = new Node(number);
+            head.next = current;
+        }
+        size++;
+    }
+
+    public void addLast(int number) {
+        Node node = new Node(number);
+        if (head == null) {
+            head = node;
+        } else if (size == 1) {
+            head.next = node;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        size++;
+    }
+
+    public int removeFirst() {
+        Node current = head;
+        if (size == 0) throw new NoSuchElementException();
+        else if (size == 1) head = null;
+        else head = current.next;
+        size--;
+
+        return current.data;
+    }
+
+    public int removeLast() {
+        Node current = head;
+        if (size == 0) throw new NoSuchElementException();
+        else if (size == 1) head = null;
+        else {
+            while (current.next != null) {
+                current = current.next;
+            }
+        }
+        size--;
+
+        return current.data;
+    }
+
+    public int size() {
+        return size;
+    }
+
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void printList() {
+        if (size == 0) throw new NullPointerException();
+        else {
+            Node current = head;
+            while (current.next != null) {
+                System.out.print(current.data + "->");
+                current = current.next;
+            }
+            System.out.print(current.data);
+        }
+    }
+
+}
+
---
+++ src/problems/collections/linkedlist/Main.java
@@ -0,0 +1,10 @@
+package problems.collections.linkedlist;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.printList();
+    }
+}
