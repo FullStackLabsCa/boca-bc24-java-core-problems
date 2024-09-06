392b853259cb5162e920048898b400435a5c2e17
Kiran <Kiran.Virani@fullstacklabs.digital>
2024-09-06 11:34:19 -0400

    implemented linked list with nodes and all tests passed.

---
+++ src/problems/linkedlist/Main.java
@@ -0,0 +1,21 @@
+package linkedlist;
+
+import arraylist.MyArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        MyLinkedList myList = new MyLinkedList();
+
+        myList.printList();
+
+        myList.addFirst(30);
+        myList.addFirst(20);
+        myList.addFirst(10);
+
+        myList.addLast(40);
+        myList.addLast(50);
+        myList.addLast(60);
+
+        myList.printList();
+    }
+}
---
+++ src/problems/linkedlist/MyLinkedList.java
@@ -0,0 +1,89 @@
+package linkedlist;
+
+import java.util.NoSuchElementException;
+
+public class MyLinkedList {
+
+    private Node head, current;
+    private int count = 0;
+
+    public MyLinkedList() {
+        this.head = null;
+        this.count = 0;
+    }
+
+    public void addFirst(int data) {
+        if (head == null) {
+            head = new Node(data, null);
+        } else {
+            head = new Node(data, head);
+        }
+        count++;
+    }
+
+    public void addLast(int data) {
+        if (head == null) {
+            head = new Node(data, null);
+        } else {
+            current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = new Node(data, null);
+        }
+        count++;
+    }
+
+    public int removeFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        int removedElement = 0;
+        removedElement = head.data;
+        head = head.next;
+        count--;
+        return removedElement;
+    }
+
+    public int removeLast() {
+        int removedElement = 0;
+        if (head == null) {
+            throw new NoSuchElementException();
+        } else if (head.next == null) {
+            removedElement = head.data;
+            head = null;
+        } else {
+            current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            removedElement = current.data;
+            count--;
+        }
+        return removedElement;
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public boolean isEmpty() {
+        if (head == null) {
+            return true;
+        }
+        return false;
+    }
+
+    public void printList() {
+        if (head == null) {
+            System.out.println("No nodes found in a linked list.");
+        } else {
+            current = head;
+            while (current.next != null) {
+                System.out.println("node 1 = " + current.data);
+                current = current.next;
+            }
+            System.out.println("last node==" + current.data);
+        }
+    }
+}
