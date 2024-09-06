f5b50973e3f2e8bfd576570b378cd1fc75ace6ae
Dhruv Desai <dhruvdesai@770@gmail.com>
2024-09-06 10:46:10 -0400

    Fixed MyArrayList problem and all Test passed successfully.

--- src/problems/array/MyArrayList.java
+++ src/problems/array/MyArrayList.java
@@ -1,46 +1,77 @@
 package problems.array;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MyArrayList <T>{
-    public ArrayList<T> list;
+    public T[] list;
+
+    @Override
+    public String toString() {
+        return "MyArrayList{" +
+                "list=" + Arrays.toString(list) +
+                '}';
+    }
+
+    public int size= 0;
 
     public MyArrayList() {
-        this.list= new ArrayList<>(10);
+        this.list= (T[]) new Object[0];
+    }
+
+    public MyArrayList(int capacity) {
+        this.list= (T[]) new Object[capacity];
     }
 
     public void increaseCapacity(){
-        ArrayList<T> temp= new ArrayList<>(list.size()+10);
-        for(int i=0; i<list.size(); i++){
-            temp.add(i, list.get(i));
+        T[] temp= (T[]) new Object[size+1];
+        for(int i=0; i<list.length; i++){
+            temp[i]= list[i];
         }
         list=temp;
     }
 
     public void add(T element) {
-        if(list.size()==10){
+        if(list.length==size){
             increaseCapacity();
         }
-        list.add(element);
+        list[size]= element;
+        size++;
     }
 
     public int size() {
-        return list.size();
+        int count= 0;
+        for(int i=0; i<list.length; i++){
+           count++;
+        }
+        return count;
     }
 
     public T get(int i) {
-        return list.get(i);
+        return list[i];
     }
 
     public T remove(int i) {
-        return list.remove(i);
+        T[] temp= (T[]) new Object[list.length-1];
+        T removedElement= list[i];
+
+        int tempIndex=0;
+        for(int j=0; j<list.length; j++){
+            if(j==i){
+                continue;
+            }
+            temp[tempIndex++]=list[j];
+        }
+        list= temp;
+        return removedElement;
     }
 
     public void clear() {
-        list.clear();
+        T[] temp= (T[]) new Object[0];
+
+        list= temp;
     }
 
     public boolean isEmpty() {
-        return list.isEmpty();
+        return list.length==0;
     }
 }
