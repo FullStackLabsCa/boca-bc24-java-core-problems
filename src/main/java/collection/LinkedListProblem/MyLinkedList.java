33147cd861ce54f4615d799e07f8da687e318130
Manpreet Kaur <Manpreet.Kaur@fsld-devwks-22.local>
2024-09-08 03:17:09 -0400

    LinkList problem done with all test passed..

--- src/main/java/collection/LinkedListProblem/MyLinkedList.java
+++ src/main/java/collection/LinkedListProblem/MyLinkedList.java
@@ -1,7 +1,8 @@
 package collection.LinkedListProblem;
+
 public class MyLinkedList<T> {
-     Node<T> head;
-     int size;
+    Node<T> head;
+    int size;
 
     public void addFirst(T element) {
         Node<T> newNode = new Node<>(element);
@@ -13,65 +14,90 @@ public class MyLinkedList<T> {
     public void addLast(T element) {
         Node<T> newNode = new Node<>(element);
         if (head == null) {
-            head=newNode;
+            head = newNode;
             size++;
-        }
-        else {
+        } else {
             Node<T> currentNode = head;
-            while(currentNode.next != null) {
+            while (currentNode.next != null) {
                 currentNode = currentNode.next;
             }
-           currentNode.next=newNode;
+            currentNode.next = newNode;
             size++;
 
         }
     }
-//
-//    public int removeFirst() {
-//        Node<T> newNode = new Node<>();
-//    }
 
     public int size() {
         return size;
     }
-//
-//    public int removeFirst() {
-//    }
-//
-//    public int removeLast() {
-//    }
-//
-//    public boolean isEmpty() {
-//        if()
-//    }
-//
+
+    public Node<T> removeFirst() {
+        if (head == null) {
+            System.out.println("nothing to remove :");
+        }
+        Node<T> newNode = head;
+        head = newNode.next;
+        size--;
+        return newNode;
+    }
+
+    public Node<T> removeLast() {
+        Node<T> lastNode = head;
+        if (head == null) {
+            System.out.println("No Node available to display");
+
+        }
+        if (head.next == null) {
+            Node<T> removeNode = head;
+            head = null;
+            size--;
+            return removeNode;
+        }
+        Node<T> currentNode = head;
+        Node<T> removedNode = currentNode.next;
+        while (currentNode.next.next != null) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = null;
+        size--;
+        return removedNode;
+    }
+
+    public boolean isEmpty() {
+        if (head == null) {
+            return true;
+        } else return false;
+    }
+
+    //
     public void printList() {
         Node<T> currentNode = head;
-        if(head==null){
+        if (head == null) {
             System.out.println("Nothing to print");
             return;
         }
-      while (currentNode!=null){
-          System.out.println(currentNode.data+"  ");
-          currentNode =currentNode.next;
+        while (currentNode != null) {
+            System.out.println(currentNode.data + "  ");
+            currentNode = currentNode.next;
         }
 
     }
-public static void main(String[] args) {
 
-    MyLinkedList<Integer> newnode = new MyLinkedList<>();
-    newnode.addFirst(1);
-    newnode.addFirst(2);
-    newnode.addLast(70);
+    public static void main(String[] args) {
 
+        MyLinkedList<Integer> newnode = new MyLinkedList<>();
+        newnode.addFirst(1);
+        newnode.addFirst(2);
+        newnode.addLast(70);
+        newnode.addLast(20);
+        newnode.addLast(50);
+        newnode.addLast(40);
+        newnode.isEmpty();
+        newnode.removeFirst();
+        newnode.removeLast();
+        newnode.printList();
 
-    newnode.addLast(20);
-    newnode.addLast(50);
-    newnode.addLast(40);
-    newnode.printList();
-
-}
-
+    }
 
 
 }
