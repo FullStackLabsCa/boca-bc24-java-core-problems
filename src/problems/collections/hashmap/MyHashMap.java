e88f8ba55586dac89bc795afbe60b375950c33ec
Gaurav Manchanda <gaurav.manchanda@fullstacklabs.digital>
2024-09-09 16:54:41 -0400

    created the basic structure of MyHashMap

---
+++ src/problems/collections/hashmap/MyHashMap.java
@@ -0,0 +1,22 @@
+package problems.collections.hashmap;
+
+public class MyHashMap<String, Integer> {
+    public void put(String apple, Integer i) {
+    }
+
+    public Integer get(String apple) {
+        return null;
+    }
+
+    public Integer remove(String apple) {
+        return null;
+    }
+
+    public boolean containsKey(String apple) {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+}
---
+++ src/problems/collections/hashmap/Node.java
@@ -0,0 +1,4 @@
+package problems.collections.hashmap;
+
+public class Node {
+}
