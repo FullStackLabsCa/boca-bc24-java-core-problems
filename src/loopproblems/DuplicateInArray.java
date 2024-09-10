eeafe397d93e4e1d2f596c9ad4dfbf5950295d1a
Gagandeep Kaur <Gagandeep.Kaur@fsld-devwks-19.local>
2024-09-10 09:44:19 -0400

    array program

---
+++ src/loopproblems/DuplicateInArray.java
@@ -0,0 +1,17 @@
+public class DuplicateInArray {
+    public static void main(String[] args) {
+        int[] array = {2, 3, 4, 5, 5, 4, 7};
+        findDuplicat(array);
+    }
+    public static void findDuplicat(int[] array){
+        for (int i=0; i<array.length;i++){
+            for (int j=i+1; j<array.length;j++){
+                if (array[i] == array[j]) {
+                    System.out.println("Duplicate Array is founded: "+array[i]);
+
+                }
+
+            }
+        }
+    }
+}
