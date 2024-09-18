36603d16589b2b719407a39c7edb0eb66a5691ca
Gaurav Manchanda <gaurav.manchanda@fullstacklabs.digital>
2024-09-18 10:24:08 -0400

    Pushing WIP changes

--- src/problems/trading/TradeRepository.java
+++ src/problems/trading/TradeRepository.java
@@ -1,9 +1,37 @@
 package problems.trading;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.concurrent.LinkedBlockingDeque;
 
 public class TradeRepository {
-    public static void insertQuery(LinkedBlockingDeque<TradeTransaction> tradingTransactionDeQueue){
+    public static void insertQuery(LinkedBlockingDeque<TradeTransaction> tradingTransactionDeQueue) throws Exception {
+        Connection connection = DatabaseConnectionPool.getConnection();
+        try {
+            connection.setAutoCommit(false);
 
+            String insertQuery = "INSERT INTO Trades (trade_id, ticker_symbol, quantity, price, trade_date) VALUES (?,?,?,?,?)";
+            PreparedStatement statement = connection.prepareStatement(insertQuery);
+
+            for (TradeTransaction tradeTransaction : tradingTransactionDeQueue) {
+                statement.setString(1, tradeTransaction.getTradeId());
+                statement.setString(2, tradeTransaction.getTickerSymbol());
+                statement.setInt(3, tradeTransaction.getQuantity());
+                statement.setDouble(4, tradeTransaction.getPrice());
+                statement.setDate(5, tradeTransaction.getTradeDate());
+                statement.addBatch();
+            }
+
+            statement.executeBatch();
+            connection.commit();
+            connection.setAutoCommit(true);
+        } catch (SQLException e) {
+            connection.rollback();
+            e.printStackTrace();
+        } catch (Exception e) {
+            connection.rollback();
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/problems/trading/TradingMainRunner.java
+++ src/problems/trading/TradingMainRunner.java
@@ -1,11 +1,12 @@
 package problems.trading;
 
+import java.sql.SQLException;
 import java.util.concurrent.LinkedBlockingDeque;
 
 public class TradingMainRunner {
     public static LinkedBlockingDeque<TradeTransaction> tradingTransactionDeQueue = new LinkedBlockingDeque<>(5000);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // read file and load data into LinkedBlockingDeque
         String filePath = "/Users/Gaurav.Manchanda/src/fullstack/trade_data_faulty.csv";
         tradingTransactionDeQueue = FileProcessor.readTransactionFileAndWriteToQueue(filePath, tradingTransactionDeQueue);
