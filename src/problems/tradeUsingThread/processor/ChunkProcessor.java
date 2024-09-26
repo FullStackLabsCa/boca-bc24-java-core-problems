c3f5b65a489cf211ed34dff1f4bc9230431fb82a
Dhruv Desai <dhruvdesai@770@gmail.com>
2024-09-26 15:26:48 -0400

    Able to insert data into trade_payload for Trade Using Multithreading Problem

--- src/problems/tradeUsingThread/processor/ChunkProcessor.java
+++ src/problems/tradeUsingThread/processor/ChunkProcessor.java
@@ -1,9 +1,13 @@
 package problems.tradeUsingThread.processor;
 
+import problems.tradeUsingThread.databaseConnection.HikariCP;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.Scanner;
 
 public class ChunkProcessor implements Runnable{
@@ -27,14 +31,38 @@ public class ChunkProcessor implements Runnable{
 
     private void readChunk() throws FileNotFoundException {
         try (Scanner scanner= new Scanner(new FileReader(filePath))){
-            scanner.useDelimiter("\n|,");
 
-//            while (scanner.hasNext()){
-//                String data = scanner.next();
-//                System.out.println("Processing data: " + data);
-//            }
+            int fieldCount= 7;
+            boolean stat= true;
+
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                String[] fields = line.split(",");
+
+                    String trade_id = fields[0];
+                    boolean status = stat;
+                    String payload = line;
+
+                    if(fields.length!=fieldCount) {
+                        status = false;
+                    }
+                insertTradePayloadData(trade_id, status, payload);
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void insertTradePayloadData(String trade_id, Boolean status, String payload) throws Exception {
+        String query= "INSERT INTO trade_payloads (trade_id, status, payload) VALUES (?, ?, ?)";
+        try (Connection connection= HikariCP.getConnection()){
+            PreparedStatement preparedStatement= connection.prepareStatement(query);
+            preparedStatement.setString(1, trade_id);
+            preparedStatement.setBoolean(2, status);
+            preparedStatement.setString(3, payload);
+            preparedStatement.executeUpdate();
         }
     }
 }
