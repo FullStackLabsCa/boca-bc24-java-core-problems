b9b9f12476fb4e649925c9e0853a17bf37867425
Gaurav.Manchanda <gaurav.manchanda@fullstacklabs.digital>
2024-09-28 22:47:06 -0400

    did the code clean up

--- src/problems/thread/trade/ThreadTradeService.java
+++ src/problems/thread/trade/ThreadTradeService.java
@@ -1,27 +1,21 @@
 package problems.thread.trade;
 
-import problems.trading.exceptions.HitErrorsThresholdException;
-
 import java.io.File;
-import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
 
+@SuppressWarnings("java:S106")
 public class ThreadTradeService {
-    public static int writeErrorCount = 0;
-    public static int errorCount = 0;
-    public static double errorThreshold = 0;
     public static File writeFile = new File("writeLogfile.txt");
     public static File readFile = new File("readLogfile.txt");
     public static boolean isFileExist = false;
-    public static boolean isThresholdValid = false;
     public static String filePath = "";
     public  static int numberOfChunksFiles = 10;
     // ANSI escape codes for colors
     public static final String ANSI_GREEN = "\u001B[32m";
-    public static LinkedBlockingQueue<String> queue1 = new LinkedBlockingQueue<>();
-    public static LinkedBlockingQueue<String> queue2 = new LinkedBlockingQueue<>();
-    public static LinkedBlockingQueue<String> queue3 = new LinkedBlockingQueue<>();
+    public static final LinkedBlockingQueue<String> queue1 = new LinkedBlockingQueue<>();
+    public static final LinkedBlockingQueue<String> queue2 = new LinkedBlockingQueue<>();
+    public static final LinkedBlockingQueue<String> queue3 = new LinkedBlockingQueue<>();
     public static final ConcurrentHashMap<String, Integer> queueDistributorConcurrentHashMap = new ConcurrentHashMap<>();
 
 
