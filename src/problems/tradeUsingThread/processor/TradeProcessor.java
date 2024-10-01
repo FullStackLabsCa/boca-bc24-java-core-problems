e237d80a4a464e8feb1379f2f010cecc29d1afca
Dhruv Desai <dhruvdesai@770@gmail.com>
2024-10-01 11:16:30 -0400

    Done with Trade Processor using Thread version 1 (v1)

--- src/problems/tradeUsingThread/processor/TradeProcessor.java
+++ src/problems/tradeUsingThread/processor/TradeProcessor.java
@@ -6,17 +6,17 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class TradeProcessor implements Runnable {
 
     String lines;
     String symbol;
-    public int totalrowsInsertedInJournalTable= 0;
+    int security_id;
+
+    static int tempPos;
+
     LinkedBlockingQueue<String> queue;
-    List<String> payload = new ArrayList<>();
 
     public TradeProcessor(LinkedBlockingQueue<String> queue) {
         this.queue = queue;
@@ -48,15 +48,13 @@ public class TradeProcessor implements Runnable {
             while (rs.next()) {
                 lines = rs.getString("payload");
                 lookSecurityTable(lines);
-//                insertToJournalTable(lines, symbol);
-
             }
         }
     }
 
     public void lookSecurityTable(String lines) {
         String[] fields = lines.split(",");
-        String query = "SELECT cusip FROM securities_reference WHERE cusip= ?";
+        String query = "SELECT cusip, security_id FROM securities_reference WHERE cusip= ?";
         try (Connection connection = HikariCP.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             preparedStatement.setString(1, fields[3]);
@@ -64,28 +62,100 @@ public class TradeProcessor implements Runnable {
 
             while (rs.next()) {
                 symbol = rs.getString("cusip");
-                insertToJournalTable(lines, symbol);
+                security_id = rs.getInt("security_id");
+                insertToJournalTable(lines, symbol, security_id);
+                getAccAndSec(lines, security_id);
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void insertToJournalTable(String lines, String symbol) {
-        String insertToJournalQuery = "INSERT INTO journal_entry (account_no, direction, quantity) VALUES (?, ?, ?)";
+    public void insertToJournalTable(String lines, String symbol, int security_id) {
+        String insertToJournalQuery = "INSERT INTO journal_entry (account_no, security_id, direction, quantity) VALUES (?, ?, ?, ?)";
         String[] fields = lines.split(",");
         if (symbol != null) {
             try (Connection connection = HikariCP.getConnection()) {
                 PreparedStatement preparedStatement = connection.prepareStatement(insertToJournalQuery);
                 preparedStatement.setString(1, fields[2]);
-                preparedStatement.setString(2, fields[4]);
-                preparedStatement.setInt(3, Integer.parseInt(fields[5]));
+                preparedStatement.setInt(2, security_id);
+                preparedStatement.setString(3, fields[4]);
+                preparedStatement.setInt(4, Integer.parseInt(fields[5]));
 
                 preparedStatement.executeUpdate();
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             } catch (Exception e) {
                 throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void getAccAndSec(String lines, int security_id) {
+        String checkPresent = "SELECT position FROM position_table WHERE account_no= ? AND security_id= ?";
+        String[] fields = lines.split(",");
+        try (Connection connection = HikariCP.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(checkPresent);
+            preparedStatement.setString(1, fields[2]);
+            preparedStatement.setInt(2, security_id);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            if (rs.next()) {
+                tempPos = rs.getInt("position");
+                update_Position_table(lines, security_id, tempPos);
+            } else {
+                insert_Position_table(lines, security_id);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void insert_Position_table(String lines, int security_id) throws Exception {
+        String insertToPositionQuery = "INSERT INTO position_table (account_no, security_id, position, version) VALUES (?, ?, ?, 0)";
+        String[] fields = lines.split(",");
+
+        if (symbol != null) {
+            try (Connection connection = HikariCP.getConnection()) {
+                PreparedStatement preparedStatement = connection.prepareStatement(insertToPositionQuery);
+                preparedStatement.setString(1, fields[2]);
+                preparedStatement.setInt(2, security_id);
+                if (fields[4].equals("SELL")) {
+                    preparedStatement.setInt(3, -Integer.parseInt(fields[5]));
+                } else {
+                    preparedStatement.setInt(3, Integer.parseInt(fields[5]));
+                }
+
+                preparedStatement.executeUpdate();
+            }
+        }
+    }
+
+    public void update_Position_table(String lines, int security_id, int tempPos) throws Exception {
+        String updateToPositionQuery = "Update position_table SET position = ? WHERE account_no= ? AND security_id= ?";
+        String[] fields = lines.split(",");
+
+        if (symbol != null) {
+            try (Connection connection = HikariCP.getConnection()) {
+                try {
+                    PreparedStatement preparedStatement = connection.prepareStatement(updateToPositionQuery);
+                    if (fields[4].equals("BUY")) {
+                        preparedStatement.setInt(1, (tempPos + Integer.parseInt(fields[5])));
+                    }
+                    if (fields[4].equals("SELL")) {
+                        preparedStatement.setInt(1, (tempPos - Integer.parseInt(fields[5])));
+                    }
+                    preparedStatement.setString(2, fields[2]);
+                    preparedStatement.setInt(3, security_id);
+
+                    preparedStatement.executeUpdate();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
             }
         }
     }
