ac5ee3889e1c88a39b62fde05f57f4bf42710de7
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-12-06 10:41:36 -0500

    Added Tests that need to be written (only structure)

--- test/cache_lib/JavaCacheTest.java
+++ test/cache_lib/JavaCacheTest.java
@@ -23,12 +23,89 @@ public class JavaCacheTest {
     }
 
     @Test
-    public void putTestWithDuration(){
+    public void putWithDurationTest(){
         assertEquals(0, cache.size());
         cache.put("akshat", "singla",10);
         assertEquals(1, cache.size());
     }
 
+    @Test
+    public void putTestRemovalAfterTTL(){
 
+    }
 
+    @Test
+    public void putWithDurationTestRemovalAfterTTL(){
+
+    }
+
+    @Test
+    public void putTestTTLResetAfterRead(){
+
+    }
+
+    @Test
+    public void getNonExistentKeyTest(){
+
+    }
+
+    @Test
+    public void getExistingKeyTest(){
+
+    }
+
+    @Test
+    public void getResetTTLTest(){
+
+    }
+
+    @Test
+    public void removeNonExistingKeyTest(){
+
+    }
+
+    @Test
+    public void removeExistingKeyTest(){
+
+    }
+
+    @Test
+    public void removeExpiredKeyTest(){
+
+    }
+
+    @Test
+    public void sizeTestWithNoElements(){
+
+    }
+
+    @Test
+    public void sizeTest(){
+
+    }
+
+    @Test
+    public void sizeBeforeTTLExpirationTest(){
+
+    }
+
+    @Test
+    public void sizeAfterTTLExpirationTest(){
+
+    }
+
+    @Test
+    public void clearTest(){
+
+    }
+
+    @Test
+    public void keysTestWithNoElements(){
+
+    }
+
+    @Test
+    public void keysTest(){
+
+    }
 }
