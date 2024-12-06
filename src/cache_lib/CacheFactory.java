b00c4a1d47198b3c42ba3faa35304fc0408fb385
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-12-06 15:36:32 -0500

    Added daemon threads Init methods in CacheFactory

--- src/cache_lib/CacheFactory.java
+++ src/cache_lib/CacheFactory.java
@@ -1,4 +1,35 @@
 package cache_lib;
 
+import java.util.Collection;
+
 public class CacheFactory {
+
+    // This factory maintains a collection of JavaObjects with Certain Policy
+    /*
+    * */
+
+    private void initTTLMonitoringForCaches(Collection<JavaCache<?,?>> cacheCollection){
+        DaemonFactory.getInstance().ttlPolicyMonitorProvider(cacheCollection);
+    }
+
+    private void initLRUMonitoringForCaches(Collection<JavaCache<?,?>> cacheCollection){
+        DaemonFactory.getInstance().LRUPolicyMonitorProvider(cacheCollection);
+    }
+
+    private void initFIFOMonitoringForCaches(Collection<JavaCache<?,?>> cacheCollection){
+        DaemonFactory.getInstance().FIFOPolicyMonitorProvider(cacheCollection);
+    }
+
+    private void initLFUMonitoringForCaches(Collection<JavaCache<?,?>> cacheCollection){
+        DaemonFactory.getInstance().LFUPolicyMonitorProvider(cacheCollection);
+    }
+
+    private void initRRMonitoringForCaches(Collection<JavaCache<?,?>> cacheCollection){
+        DaemonFactory.getInstance().RRPolicyMonitorProvider(cacheCollection);
+    }
+
+    private void initSizeBasedEvictionMonitoringForCaches(Collection<JavaCache<?,?>> cacheCollection){
+        DaemonFactory.getInstance().sizeBasedEvictionPolicyMonitorProvider(cacheCollection);
+    }
+
 }
