0903a82d0dfe562b42aa370b66b118d272b1ce10
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-12-09 12:21:45 -0500

    Cache Library - LRU - Only updating the key removal when size threshold is reached

--- src/cache_lib/DaemonFactory.java
+++ src/cache_lib/DaemonFactory.java
@@ -55,10 +55,12 @@ public class DaemonFactory {
                             // Iterate over data entries in a Cache
                             javaCache.getValues().iterator().forEachRemaining(
                                     dataEntry -> {
-                                        Duration duration = Duration.between(dataEntry.getLastAccessTime(), LocalDateTime.now());
-                                        if (leastAccessTime[0] == null || duration.getSeconds() < leastAccessTime[0].getSeconds()) {
-                                            leastAccessTime[0] = duration;
-                                            key[0] = dataEntry.getKey();
+                                        if(javaCache.size() >= dataEntry.getMaxSizePermitted()) {
+                                            Duration duration = Duration.between(dataEntry.getLastAccessTime(), LocalDateTime.now());
+                                            if (leastAccessTime[0] == null || duration.getSeconds() < leastAccessTime[0].getSeconds()) {
+                                                leastAccessTime[0] = duration;
+                                                key[0] = dataEntry.getKey();
+                                            }
                                         }
                                     });
                             javaCache.remove(key);
