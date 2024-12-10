e258724fb6302da1f83cb2c8be41eb5dddd3cbf1
Akshat-Singla <akshat.singla@fullstacklabs.digital>
2024-12-10 11:14:58 -0500

    Cache Library - LFU Testing - added tests for LFU

--- test/cache_lib/JavaCacheTest.java
+++ test/cache_lib/JavaCacheTest.java
@@ -394,22 +394,93 @@ class JavaCacheTest {
 
 //LFU Tests
     @Test
-    void lfuSizeTest(){
+    void lfuSizeTest() throws InterruptedException {
+        //SetUp
+        for (int i = 0; i <= 9; i++) {
+            lfuCache.put("key".concat(String.valueOf(i)), "value");
+        }
+        for (int i = 0; i <= 8; i++) {
+            lfuCache.get("key".concat(String.valueOf(i)));
+        }
+        lfuCache.put("key10", "value");
+        lfuCache.put("key11", "value");
+        Thread.sleep(1000); // Allow Daemon Thread to iterate through
 
+        //Assertions
+        assertEquals(10, lfuCache.size());
     }
 
     @Test
-    void lfuGetTest(){
+    void lfuGetTest() throws InterruptedException {
+        //SetUp
+        for (int i = 0; i <= 9; i++) {
+            lfuCache.put("key".concat(String.valueOf(i)), "value");
+        }
+        for (int i = 0; i <= 8; i++) {
+            lfuCache.get("key".concat(String.valueOf(i)));
+        }
+        lfuCache.put("key10", "value");
+        lfuCache.put("key11", "value");
+        lfuCache.get("key11");
+        Thread.sleep(1000); // Allow Daemon Thread to iterate through
 
+        //Assertions
+        for (int i = 0; i <= 8; i++) {
+            assertNotNull(lfuCache.get("key".concat(String.valueOf(i))));
+        }
+        assertNull(lfuCache.get("key9"));
+        assertNull(lfuCache.get("key10"));
+        for (int i = 0; i <= 8; i++) {
+            assertEquals("value", lfuCache.get("key".concat(String.valueOf(i))));
+        }
+        assertEquals("value", lfuCache.get("key11"));
     }
 
     @Test
-    void lfuKeysTest(){
+    void lfuKeysTest() throws InterruptedException {
+        //SetUp
+        for (int i = 0; i <= 9; i++) {
+            lfuCache.put("key".concat(String.valueOf(i)), "value");
+        }
+        for (int i = 0; i <= 8; i++) {
+            lfuCache.get("key".concat(String.valueOf(i)));
+        }
+        lfuCache.put("key10", "value");
+        lfuCache.put("key11", "value");
+        lfuCache.get("key11");
+        Thread.sleep(1000); // Allow Daemon Thread to iterate through
 
+        //Assertions
+        for (int i = 0; i <= 8; i++) {
+            assertTrue(lfuCache.keys().contains("key".concat(String.valueOf(i))));
+        }
+        assertTrue(lfuCache.keys().contains("key11"));
+        assertFalse(lfuCache.keys().contains("key9"));
+        assertFalse(lfuCache.keys().contains("key10"));
     }
 
     @Test
-    void lfuRemoveTest(){
+    void lfuRemoveTest() throws InterruptedException {
+        //SetUp
+        for (int i = 0; i <= 9; i++) {
+            lfuCache.put("key".concat(String.valueOf(i)), "value");
+        }
+        for (int i = 0; i <= 8; i++) {
+            lfuCache.get("key".concat(String.valueOf(i)));
+        }
+        lfuCache.put("key10", "value");
+        lfuCache.put("key11", "value");
+        lfuCache.get("key11");
+        Thread.sleep(1000); // Allow Daemon Thread to iterate through
 
+        //Assertions
+        for (int i = 0; i <= 8; i++) {
+            assertTrue(lfuCache.remove("key".concat(String.valueOf(i))));
+        }
+        for (int i = 0; i <= 9; i++) {
+            assertFalse(lfuCache.keys().contains("key".concat(String.valueOf(i))));
+        }
+        assertFalse(lfuCache.remove("key9"));
+        assertFalse(lfuCache.remove("key10"));
     }
 }
